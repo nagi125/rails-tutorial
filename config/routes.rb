9f806aaef26584b58465520e1101cd46fb8cf281
nagi125 <nagi125.dev@gmail.com>
2019-07-27 15:17:06 +0900

    finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  # User
+  get 'users/new'
+  get '/signup',  to: 'users#new'
+
+  # static_pages
   root 'static_pages#home'
   get '/help',    to: 'static_pages#help'
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+
   get 'foo/bar'
   get 'foo/baz'
   resources :microposts
